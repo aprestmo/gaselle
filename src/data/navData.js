59d4d11a6d4bed157ffef19cd38baaab184ef209
Alexander Prestmo <aprestmo@gmail.com>
2023-05-08 09:22:50 +0200

    Add single route

--- src/data/navData.js
+++ src/data/navData.js
@@ -1,7 +1,27 @@
 const navData = [
   {
-    name: '',
+    name: 'Forsiden',
     path: '/',
+  },
+  {
+    name: 'Nyheter',
+    path: '/nyheter/',
+  },
+  {
+    name: 'Community',
+    path: '/community/',
+  },
+  {
+    name: 'Vinnere',
+    path: '/vinnere/',
+  },
+  {
+    name: 'Om Gaselle',
+    path: '/om-gaselle/',
+  },
+  {
+    name: 'Min profil',
+    path: '/min-profil/',
   },
 ]
 

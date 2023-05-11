12decfeba818aa9a691b2b8b6c782757abd9b413
Alexander Prestmo <aprestmo@gmail.com>
2023-05-11 15:05:58 +0200

    Add netlify adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,17 +1,23 @@
 import { defineConfig } from "astro/config";
 import sanity from "astro-sanity";
 import netlify from "@astrojs/netlify/functions";
+import node from "@astrojs/node";
+import astroAws from "@astro-aws/adapter";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [sanity({
-    projectId: "4awmkexx",
-    dataset: "development",
-    apiVersion: "2023-05-09",
-    useCdn: true
-  }), compress()],
-  site: "https://gaselle-2023.netlify.app"
+  site: "https://www.dn.no",
+  base: "prosjekter/gaselle-2023/",
+  integrations: [
+    sanity({
+      projectId: "4awmkexx",
+      dataset: "development",
+      apiVersion: "2023-05-09",
+      useCdn: true,
+    }),
+    // compress()
+  ],
 });
